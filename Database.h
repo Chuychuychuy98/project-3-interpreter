cf5bfc4a907682ccfb25d25a72ac49c99c731dd5
Andenor <rowebot98@gmail.com>
2021-11-02 23:28:07 -0600

    Change relations to relation pointers

--- Database.h
+++ Database.h
@@ -10,15 +10,15 @@
 class Database {
 public:
     Database();
-    void AddRelation(const Relation &rel) {relations.insert(rel);}
-    bool ContainsRelation(const Relation &rel) {return relations.find(rel) != relations.end();}
-    void RemoveRelation(const Relation &rel) {relations.erase(rel);}
-    Relation GetRelation(const std::string &name) {return names.at(name);}
+    void AddRelation(Relation *rel) {relations.insert(rel);}
+    bool ContainsRelation(Relation *rel) {return relations.find(rel) != relations.end();}
+    void RemoveRelation(Relation *rel) {relations.erase(rel);}
+    Relation* GetRelation(const std::string &name) {return names.at(name);}
 
 
 private:
-    std::set<Relation> relations;
-    std::map<std::string, Relation> names;
+    std::set<Relation*> relations;
+    std::map<std::string, Relation*> names;
 };
 
 
