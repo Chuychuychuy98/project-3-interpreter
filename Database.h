0a1989b57c039533fc0da0efe9e2831c279b01e7
Andenor <rowebot98@gmail.com>
2021-11-04 17:06:36 -0600

    Remove "relations" set, change "names" map to "relations", use map instead of set

--- Database.h
+++ Database.h
@@ -9,16 +9,19 @@
 #include "Relation.h"
 class Database {
 public:
-    Database();
-    void AddRelation(Relation *rel) {relations.insert(rel);}
-    bool ContainsRelation(Relation *rel) {return relations.find(rel) != relations.end();}
-    void RemoveRelation(Relation *rel) {relations.erase(rel);}
-    Relation* GetRelation(const std::string &name) {return names.at(name);}
-
+    Database() = default;
+    void AddRelation(std::string name, Relation *rel) {
+        //relations.insert(rel);
+        relations.insert(std::pair<std::string, Relation*>(name, rel));
+    }
+    bool ContainsRelation(const std::string &name) {return relations.find(name) != relations.end();}
+    void RemoveRelation(const std::string &name) {relations.erase(name);}
+    Relation* GetRelation(const std::string &name) {return relations.at(name);}
+    std::map<std::string, Relation*> GetRelations() {return relations;}
 
 private:
-    std::set<Relation*> relations;
-    std::map<std::string, Relation*> names;
+    //std::set<Relation*> relations;
+    std::map<std::string, Relation*> relations;
 };
 
 
