a91e7c7e84ee4e6f8750816be2321db28e379caf
Andenor <rowebot98@gmail.com>
2021-11-04 17:04:51 -0600

    Modify Project, Select, and Rename to return an object instead of a pointer

--- Relation.cpp
+++ Relation.cpp
@@ -5,30 +5,65 @@
 #include "Relation.h"
 
 std::string Relation::ToString() {
+    std::string toReturn = ToStringHeader();
+    toReturn += ToStringTups();
+    return toReturn;
+}
+
+std::string Relation::GetAnswer() {
     std::string toReturn;
-    for (Tuple tup : tuples) {
-        for (int i = 0; i < header->Size(); ++i) {
-            toReturn += header->GetVal(i) + "=" + tup.GetVal(i);
-            if (i < header->Size() - 1) {
-                toReturn += ", ";
+    if (tuples.empty()) {
+        toReturn += "No\n";
+        return toReturn;
+    }
+    toReturn += "Yes(" + std::to_string(tuples.size()) + ")\n";
+    return toReturn;
+}
+
+std::string Relation::ToStringHeader() {
+    std::string toReturn = name + "(";
+    for (int i = 0; i < header.Size(); ++i) {
+        toReturn += header.GetVal(i);
+        if (i < header.Size() - 1) {
+            toReturn += ",";
+        }
+    }
+    toReturn += ")? ";
+
+    toReturn += GetAnswer();
+    return toReturn;
+}
+
+std::string Relation::ToStringTups() {
+    std::string toReturn;
+    for (auto tup : tuples) {
+        if (tup.Size() == 0) {
+            break;
+        }
+        toReturn += "  ";
+        for (int i = 0; i < tup.Size(); ++i) {
+            toReturn += header.GetVal(i) + "=" + tup.GetVal(i);
+            if (i < tup.Size() - 1) {
+                toReturn += ",";
             }
         }
         toReturn += "\n";
     }
+
     return toReturn;
 }
 
-Relation* Relation::Select(int i, std::string val) {
-    Relation* newRel= new Relation(name, header);
+Relation Relation::Select(int i, std::string val) {
+    Relation newRel(name, header);
     for (Tuple tup : tuples) {
         if (tup.GetVal(i) == val) {
-            newRel->AddTuple(tup);
+            newRel.AddTuple(tup);
         }
     }
     return newRel;
 }
-Relation* Relation::Select(int i, int j) {
-    Relation* newRel = new Relation(name, header);
+Relation Relation::Select(int i, int j) {
+    Relation newRel(name, header);
     for (Tuple tup : tuples) {
         if (tup.GetVal(i) == tup.GetVal(j)) {
             newRel.AddTuple(tup);
@@ -37,23 +72,30 @@ Relation* Relation::Select(int i, int j) {
     return newRel;
 }
 
-Relation* Relation::Project(std::initializer_list<int> indices) {
+Relation Relation::Project(std::vector<int> indices) {
+    std::vector<std::string> vals;
+    for (int i : indices) {
+        vals.push_back(header.GetVal(i));
+    }
+    Header newHead(vals);
+    Relation r(name, newHead);
+
     for (Tuple tup : tuples) {
         Tuple newTup;
         for (int i : indices) {
             newTup.AddVal(tup.GetVal(i));
         }
+        r.AddTuple(newTup);
     }
-    std::vector<std::string> vals;
-    for (int i : indices) {
-        vals.push_back(header->GetVal(i));
-    }
-    Header* newHead = new Header(vals);
-    return new Relation(name, newHead);
+    return r;
 }
 
-Relation* Relation::Rename(std::initializer_list<std::string> newAttr) {
+Relation Relation::Rename(std::vector<std::string> newAttr) {
 
-    Header *newHeader = new Header(newAttr);
-    return new Relation(name, newHeader);
+    Header newHeader(newAttr);
+    Relation r(name, newHeader);
+    for (auto t : tuples) {
+        r.AddTuple(t);
+    }
+    return r;
 }
--- Relation.h
+++ Relation.h
@@ -5,7 +5,6 @@
 #ifndef PROJECT_3_INTERPRETER_RELATION_H
 #define PROJECT_3_INTERPRETER_RELATION_H
 #include <set>
-#include <initializer_list>
 #include "Tuple.h"
 #include "Header.h"
 
@@ -14,11 +13,14 @@ class Relation {
 public:
     Relation(std::string name, Header* header) : name(std::move(name)), header(header) {}
     void AddTuple(Tuple tup) {if (tup.Size() == header.Size()) tuples.insert(tup);}
-    Relation *Select(int i, std::string val);
-    Relation *Select(int i, int j);
+    Relation Select(int i, std::string val);
+    Relation Select(int i, int j);
     std::string ToString();
-    Relation *Project(std::initializer_list<int> indices); // list of indices
-    Relation *Rename(std::initializer_list<std::string> newAttr); // list of attributes
+    std::string ToStringHeader();
+    std::string ToStringTups();
+    std::string GetAnswer();
+    Relation Project(std::vector<int> indices); // list of indices
+    Relation Rename(std::vector<std::string> newAttr); // list of attributes
 
 private:
     std::set<Tuple> tuples;
